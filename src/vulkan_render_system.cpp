dc87bceb9110ab8a13a2f16ec2f82cd22f4c0fb7
hexinchen <hexinchen7731@navinfo.com>
2022-03-10 19:10:17 +0800

    tweak

--- src/vulkan_render_system.cpp
+++ src/vulkan_render_system.cpp
@@ -348,6 +348,22 @@ namespace rs
 		vkDestroyShaderModule(m_logicDevice, fragShaderModule, nullptr);
 	}
 
+	void VulkanRenderSystem::createRenderPass()
+	{
+		VkAttachmentDescription colorAttachment{};
+		colorAttachment.format = m_swapChainImageFormat;
+		colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+		colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+		VkAttachmentReference colorAttachmentRef{};
+		colorAttachmentRef.attachment = 0;
+		colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	}
+
 	void VulkanRenderSystem::setSurfaceHandle(GLFWwindow* window)
 	{
 		m_window = window;
